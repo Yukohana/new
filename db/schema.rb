e6a4332ec6129ca74f3c8756c1e3aa4bd2c453cf
yuko <jpsky@outlook.jp>
2021-05-30 12:40:17 +0000

    Finish add-basic-info

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210525120724) do
+ActiveRecord::Schema.define(version: 20210529020218) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,9 @@ ActiveRecord::Schema.define(version: 20210525120724) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.string "department"
+    t.datetime "basic_time", default: "2021-05-29 23:00:00"
+    t.datetime "work_time", default: "2021-05-29 22:30:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
